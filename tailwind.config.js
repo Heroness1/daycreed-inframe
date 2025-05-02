d82354132d2d60a841035d3228e86e7e46720ff4
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-03 00:45:01 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class', // Enable dark mode via class
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
   },
   plugins: [],
 }
