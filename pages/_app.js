9c6037612df7964565bc6d2cca39b9024b4a73c2
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2026-09-07 17:53:41 +0700

    Add Sora font and refactor App component
    Integrate Sora font into the application and update structure.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
-import '../styles/globals.css'
-import { ThemeProvider } from '../context/ThemeContext'
+import { Sora } from "next/font/google";
+import "../styles/globals.css"; // sesuaikan path-nya
+
+const sora = Sora({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // sesuaikan weight yang kamu butuhkan
+  display: "swap",
+  variable: "--font-sora",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider>
+    <div className={sora.variable}>
       <Component {...pageProps} />
-    </ThemeProvider>
-  )
+    </div>
+  );
 }
