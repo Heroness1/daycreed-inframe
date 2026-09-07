a6487b8097483c42393f2596587a3297f968fa1f
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2026-09-07 17:52:15 +0700

    Remove Google Fonts link from _document.js
    Removed Google Fonts link from the document head.

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,7 @@ export default function Document() {
   return (
     <Html lang="id">
       <Head>
-        {/* Google Fonts - Playfair Display (premium heading) + Inter */}
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Playfair+Display:wght@700&display=swap" 
-          rel="stylesheet" 
-        />
+        
       </Head>
       <body>
         <Main />
